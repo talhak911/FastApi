53c93562fd97d8c9d9da3d0ac7851cb49b3f30cb
talhak911 <talhakhan8300@gmail.com>
2024-03-25 17:58:27 +0500

    fastapi

--- fastApi (CRUD operations in memory)/FastApiy/fastapiy/main.py
+++ fastApi (CRUD operations in memory)/FastApiy/fastapiy/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+students =[{"name":"talha","rollNo":1},{"name":"khan","rollNo":2},{"name":"taf","rollNo":3}]
 app=FastAPI()
 @app.get("/")
 def hello():
     return "hello"
-@app.get("/students")
-def hello():
-    return "talha"
-
+@app.get("/students/{name}")
+async def GetRollNo(name:str):
+    for student in students:
+        if student["name"] ==name:
+            return f"Student roll no is {student['rollNo']}"
+@app.post("/students/")
+async def GetRollNo(name:str):
+    students.append({"name":name,"rollNo":4})
+    return students
 def start():
     uvicorn.run("fastapiy.main:app",host="127.0.0.1",port=8080,reload=True)
